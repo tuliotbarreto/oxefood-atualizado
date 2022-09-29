2533717f8fccdf4c3947e62afcad6df6ffc3fb46
aluno <aluno@ifpe>
2022-09-29 09:12:47 -0300

    Primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefoodtalitha/OxefoodTalithaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodtalitha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodTalithaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodTalithaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodtalitha/OxefoodTalithaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodtalitha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodTalithaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
